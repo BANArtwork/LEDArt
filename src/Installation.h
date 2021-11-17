dfc31bb4b31602279faa88fc40111e4d588e0f13
Chris Estes <chris.estes@dexcom.com>
2021-11-16 19:06:01 -0500

    Mapped out segments for Heavenly

--- src/Installation.h
+++ src/Installation.h
@@ -8,24 +8,28 @@
 #include "./Hardware/LedMap.h"
 
 // Total number of LEDs in the piece.
-const int numLeds = 307;
+const int numLeds = 212;
 
 // A segment of all the LEDs.
 const Segment allLedsSegment = Segment(0, numLeds);
 
 // Individual segments.
-const int numSegments = 10;
+const int numSegments = 14;
 const Segment* segments[numSegments] = {
-    new Segment(0, 30),
-    new Segment(30, 19),
-    new Segment(49, 38),
-    new Segment(87, 31),
-    new Segment(118, 30),
-    new Segment(148, 29),
-    new Segment(177, 31),
-    new Segment(208, 59),
-    new Segment(267, 19),
-    new Segment(286, 21),
+    new Segment(0, 4),
+    new Segment(4, 7),
+    new Segment(11, 6),
+    new Segment(17, 4),
+    new Segment(21, 64),
+    new Segment(85, 4),
+    new Segment(89, 7),
+    new Segment(96, 5),
+    new Segment(101, 6),
+    new Segment(107, 4),
+    new Segment(111, 7),
+    new Segment(118, 6),
+    new Segment(124, 81),
+    new Segment(205, 7),
 };
 
 // Maps of the physical coordinates of each LED.
