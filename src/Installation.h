17d6b3ef008e2682051d622dcdb8af838de13817
Chris Estes <chris.estes@dexcom.com>
2021-11-23 20:58:23 -0500

    Define LEDs

--- src/Installation.h
+++ src/Installation.h
@@ -8,16 +8,17 @@
 #include "./Hardware/LedMap.h"
 
 // Total number of LEDs in the piece.
-const int numLeds = 48;
+const int numLeds = 50;
 
 // A segment of all the LEDs.
 const Segment allLedsSegment = Segment(0, numLeds);
 
 // Individual segments.
-const int numSegments = 2;
+const int numSegments = 3;
 const Segment* segments[numSegments] = {
-    new Segment(0, 20),
-    new Segment(20, 28)
+    new Segment(0, 21),
+    new Segment(21, 21),
+    new Segment(42, 8)
 };
 
 // Maps of the physical coordinates of each LED.
