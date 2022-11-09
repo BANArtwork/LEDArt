ad15082bca8379ccec9af30e92ca7cdfb8059aec
Chris Estes <chris.estes@dexcom.com>
2022-11-09 17:22:28 -0500

    Adding new effects for KQ sign

--- src/main.cpp
+++ src/main.cpp
@@ -19,16 +19,39 @@
 #include "Effects/WhatADayAwayEffect.h"
 #include "Effects/ChasingEffect.h"
 
+#define BUTTON_PIN 9
+
 void updateLeds(int frame);
 void checkSegments(const Segment** segs, int numSegs, uint32_t c1, uint32_t c2);
 
 // List of LEDs.
 LinkedList<EffectLed*> leds = LinkedList<EffectLed*>();
 
+// Dim effects.
+Effect* dim1;
+Effect* dim2;
+Effect* dim3;
+Effect* rainbow;
+Effect* pink;
+Effect* chase;
+Effect* sparkles;
+
+void deactivateAll() {
+    dim1->deactivate();
+    dim2->deactivate();
+    dim3->deactivate();
+    rainbow->deactivate();
+    pink->deactivate();
+    chase->deactivate();
+    sparkles->deactivate();
+}
+
 void setup() {
 
     // Set up logger.
     logSetup();
+
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
 
     // Start the LED driver.
     ledDriver.begin();
@@ -56,41 +79,37 @@ void setup() {
     // Update to apply black effect.
     updateLeds(0);
 
-    auto rainbow = new ChasingRainbowEffect(5, 256, 5);
-    auto dim = new DimEffect(1);
-    auto chase = new ChasingEffect(6, 60, 0x7f7f7f, 5);
+     rainbow = (Effect*) new ChasingRainbowEffect(5, 256, 5);
+    dim1 = (Effect*) new DimEffect(1);
+    dim2 = (Effect*) new DimEffect(2);
+    dim3 = (Effect*) new DimEffect(4);
+    pink = (Effect*) new SolidColorEffect(0xfc03c6);
+    uint32_t redSparkle[1] = { 0xff0000 };
+    sparkles = (Effect*) new SparkleEffect(1, 5, 20, 1, redSparkle);
+    dim2->deactivate();
+    dim3->deactivate();
+
+     chase = (Effect*) new ChasingEffect(6, 60, 0x7f7f7f, 5);
 
     
-        allLedsSegment.forEach([rainbow, dim, chase](int index) {
+        allLedsSegment.forEach([rainbow, chase, pink, sparkles, dim1, dim2, dim3 ](int index) {
 
             leds[index]->removeEffect(0);
-            leds[index]->addEffect((Effect*)rainbow);
-            auto sparkle = new SparkleEffect(1, 400, 500);
-            leds[index]->addEffect((Effect*)dim);
+
+            leds[index]->addEffect(rainbow);
+            leds[index]->addEffect(chase);
+
+            leds[index]->addEffect(pink);
+
+       //     auto sparkle = new SparkleEffect(1, 400, 500);
+            leds[index]->addEffect((Effect*)dim1);
+            leds[index]->addEffect((Effect*)dim2);
+            leds[index]->addEffect((Effect*)dim3);
             //leds[index]->addEffect((Effect*)sparkle);
-            leds[index]->addEffect((Effect*)chase);
+
+            leds[index]->addEffect(sparkles);
         });
     
-    // for (int i = 0; i < numStars; i++) {
-    //     auto s = stars[i];
-    //     //auto blink = new BlinkEffect(0xffffff, 500, 500);
-    //     auto solid = new SolidColorEffect(0xffffff);
-    //     s->forEach([ solid](int index) {
-    //         leds[index]->removeEffect(0);
-    //         leds[index]->removeEffect(1);
-    //         //leds[index]->removeEffect(2);
-    //         leds[index]->addEffect((Effect*)solid);
-    //     });
-    // };
-
-    // allLedsSegment.forEach([rainbow, dim](int index) {
-    //     leds[index]->removeEffect(0);
-    //     leds[index]->addEffect((Effect*)rainbow);
-    //     auto sparkle = new SparkleEffect(10, 1500, rand());
-    //     leds[index]->addEffect((Effect*)sparkle);
-    //     leds[index]->addEffect((Effect*)dim);
-    // });
-
     // Check to help map segments.
     //checkSegments(segments, numSegments, 0x000000ff, 0x00ff00ff);
     //checkSegments(stars, numStars, 0x00ff0000, 0x0000ff00);
@@ -152,8 +171,69 @@ void checkLeds() {
     //}
 }
 
+void switchModes(int mode) {
+
+    deactivateAll();
+
+    switch(mode) {
+        case 0:
+        case 1:
+        case 2:
+            rainbow->activate();
+            chase->activate();
+            break;
+
+        case 3:
+        case 4:
+        case 5:
+            pink->activate();
+            sparkles->activate();
+            break;
+    }
+
+    switch(mode) {
+        case 0:
+        case 3:
+            dim1->activate();
+            break;
+
+        case 1:
+        case 4:
+            dim2->activate();
+            break;
+
+        case 2:
+        case 5:
+            dim3->activate();
+            break;
+    }
+}
+
+void checkButton() {
+
+    static int startTime = 0;
+    static bool pressing = false;
+    static int mode = 3;
+
+
+    if (!digitalRead(BUTTON_PIN)) {
+        if (!pressing) {
+            startTime = millis();
+            pressing = true;
+        } else if (millis() - startTime > 200) {
+            mode++;
+            mode = mode % 6;
+            switchModes(mode);
+            pressing = false;
+        }
+    } else {
+        pressing = false;
+    }
+}
+
 void loop() {
    checkLeds();
+   checkButton();
 }
 
 
